f4ae9e643d22e2b3e956a592fbd81b66829c210b
Shimona Elora <umysharma@gmail.com>
2017-11-22 14:59:52 +0530

    Day-night cycle

---
+++ Assets/Scripts/LightRotationScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightRotationScript : MonoBehaviour {
+
+    float initYAngle;
+    float initXAngle;
+    public float ySpeed;
+    public float xSpeed;
+
+	// Use this for initialization
+	void Start () {
+        initXAngle = GetComponent<Transform>().rotation.x;
+        initXAngle = GetComponent<Transform>().rotation.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        initXAngle = initXAngle + 0.0001f * xSpeed;
+        initYAngle = initYAngle + 0.0001f * ySpeed;
+        transform.Rotate(0.1f * xSpeed, 0.1f * ySpeed, 0);
+        //GetComponent<Transform>().rotation = new Quaternion(initXAngle, initYAngle, GetComponent<Transform>().rotation.z, GetComponent<Transform>().rotation.w);
+	}
+}
